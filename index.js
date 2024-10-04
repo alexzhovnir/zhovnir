4f9e921ee4fb50903f403b06ec7024b266f4bc63
Alex Zhovnir <zhovnirovych@gmail.com>
2024-10-04 22:29:30 +0200

    add 3d scene

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+document.addEventListener("DOMContentLoaded", function () {
+  const heroTitle = document.getElementById("hero-title");
+  const heroSubtitle = document.getElementById("hero-subtitle");
+
+  // Изменим заголовок и подзаголовок героя
+  heroTitle.textContent = "Добро пожаловать на Copyseo.ai!";
+  heroSubtitle.textContent =
+    "Создавайте SEO-оптимизированный контент с легкостью";
+});
